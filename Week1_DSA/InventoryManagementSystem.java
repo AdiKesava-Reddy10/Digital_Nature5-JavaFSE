69a0cdfda4f06d6c82dd0e1f51bca1cc6b882406
AdiKesava Reddy <vtu25083@veltech.edu.in>
2026-06-25 00:07:32 +0530

    Update InventoryManagementSystem.java

--- Week1_DSA/InventoryManagementSystem.java
+++ Week1_DSA/InventoryManagementSystem.java
@@ -1,1 +1,29 @@
-public class InventoryManagementSystem { public static void main(String[] args){ System.out.println("InventoryManagementSystem.java"); } }
+import java.util.HashMap;
+
+class Product {
+    int productId;
+    String productName;
+
+    Product(int id, String name) {
+        productId = id;
+        productName = name;
+    }
+}
+
+public class InventoryManagementSystem {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, Product> inventory =
+                new HashMap<>();
+
+        inventory.put(1, new Product(1, "Laptop"));
+        inventory.put(2, new Product(2, "Mouse"));
+
+        System.out.println("Products Added");
+
+        inventory.remove(2);
+
+        System.out.println("Product Deleted");
+    }
+}
