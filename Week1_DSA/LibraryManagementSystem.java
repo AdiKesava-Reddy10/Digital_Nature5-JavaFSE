773057f8723fdd0772e9b27fcfa2dfd5b7a708b8
AdiKesava Reddy <vtu25083@veltech.edu.in>
2026-06-25 00:10:48 +0530

    Update LibraryManagementSystem.java

--- Week1_DSA/LibraryManagementSystem.java
+++ Week1_DSA/LibraryManagementSystem.java
@@ -1,1 +1,22 @@
-public class LibraryManagementSystem { public static void main(String[] args){ System.out.println("LibraryManagementSystem.java"); } }
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+
+        String books[] = {
+                "Java",
+                "Python",
+                "DSA"
+        };
+
+        String search = "Python";
+
+        for(String book : books) {
+
+            if(book.equals(search)) {
+
+                System.out.println(
+                        "Book Found");
+            }
+        }
+    }
+}
